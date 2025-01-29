f6feb57725328c0208eb81a61226ae4440d7d02c
foysal777 <foysal.cse11@gmail.com>
2025-01-29 20:44:00 +0600

    Add Context

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CountContext } from './Service'
 
 const Home = () => {
+
+ const fetchContext = useContext(CountContext)
+
   return (
     <div>
       
      <h1 className='text-center text-6xl text-blue-700 p-16'>Welcome to Homepage in React </h1>
       
-      
+      <h1 className='text-center text-sky-800'>Your Fetching count is : {fetchContext} </h1>
       </div>
   )
 }
--- src/components/Pages/Service.jsx
+++ src/components/Pages/Service.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
+import Home from './Home';
+
+
+export const CountContext = createContext();
 
 const Service = () => {
+  const [count, setCount] = useState(0);
+  const Clicked = () => {
+    const current = count + 1;
+    setCount(current);
+    console.log(current)
+  }
+
   return (
-    <div>Service</div>
+
+    <div>
+
+      <CountContext.Provider value = {count} > 
+
+        <div className='text-center font-bold text-5xl pt-10'>
+          <button onClick={Clicked} className='bg-green-500  border-2 px-6 py-3' > Count is : {count} </button>
+          </div>
+          <Home></Home>
+      </CountContext.Provider>
+  
+
+    </div >
   )
 }
 
