3c333cda4e39f72ee77820dc4ada23185d46c82b
foysal777 <foysal.cse11@gmail.com>
2025-02-09 16:53:47 +0600

    3 step

--- src/components/ChildToParent/ChildToParentDataPass.jsx
+++ src/components/ChildToParent/ChildToParentDataPass.jsx
@@ -10,3 +10,34 @@ const ChildComponent = (props) => {
 }
 
 export default ChildComponent
+
+
+
+
+
+
+
+
+
+
+
+
+// ১. Parent Component-এ Callback Function তৈরি করা
+// ২. সেই Callback Function-টি Props হিসেবে Child Component-এ পাঠানো
+// ৩. Child Component-এ সেই Callback Function-কে Call করা
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
